aa19eaf896ee86ea21e88f4d628ec17d251b10a7
Alibek Kulzhabayev <alibek.kulzhabayev@sumup.com>
2020-11-11 17:40:55 +0100

    changed to terraform version 0.12.16

--- networking/vpc-mgmt/main.tf
+++ networking/vpc-mgmt/main.tf
@@ -14,7 +14,7 @@ terraform {
   backend "s3" {}
   # Only allow this Terraform version. Note that if you upgrade to a newer version, Terraform won't allow you to use an
   # older version, so when you upgrade, you should upgrade everyone on your team and your CI servers all at once.
-  required_version = "= 0.13.5"
+  required_version = "= 0.12.16"
 }
 module "vpc" {
   # Make sure to replace <VERSION> in this URL with the latest module-vpc release
